5f330e3f579e717bb779ce3151db450ef68f9849
tomato2718 <qweasdzxc030@gmail.com>
2025-07-18 16:27:03 +0800

    Modify: Use i32 for Shape trait

--- src/canvas/traits.rs
+++ src/canvas/traits.rs
@@ -3,13 +3,13 @@ use image::DynamicImage;
 pub trait Shape {
     fn color(&self) -> [u8; 4];
 
-    fn center(&self) -> [u32; 2];
+    fn center(&self) -> (i32, i32);
+
+    fn width(&self) -> i32;
+
+    fn height(&self) -> i32;
 
     fn z_index(&self) -> u32;
-
-    fn width(&self) -> u32;
-
-    fn height(&self) -> u32;
 }
 
 pub trait Placeable {
