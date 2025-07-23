16ecd9a6b7288fefc275fa73d5eeae49bdafec42
tomato2718 <qweasdzxc030@gmail.com>
2025-07-23 14:39:22 +0800

    Feat: Create image fitness function

--- src/image2ga/fitness.rs
+++ src/image2ga/fitness.rs
@@ -1,1 +1,42 @@
+use image::RgbaImage;
 
+pub struct ImageFitness<'a> {
+    base: &'a RgbaImage,
+}
+
+impl<'a> ImageFitness<'a> {
+    pub fn new(base: &'a RgbaImage) -> Self {
+        ImageFitness { base }
+    }
+
+    pub fn calculate(&self, target: &RgbaImage) -> u32 {
+        let base_pixels = self.base.pixels();
+        let target_pixels = target.pixels();
+        let mut score = 0;
+        for (bp, tp) in base_pixels.zip(target_pixels) {
+            score +=
+                bp.0.iter()
+                    .zip(tp.0.iter())
+                    .map(|(a, b)| (*a as i32 - *b as i32).abs() as u32)
+                    .sum::<u32>();
+        }
+        score
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use image::Rgba;
+
+    #[test]
+    fn given_base_and_target_should_return_fitness_score() {
+        let base = RgbaImage::from_pixel(32, 32, Rgba([255, 0, 0, 100]));
+        let target = RgbaImage::from_pixel(32, 32, Rgba([0, 255, 0, 100]));
+        let fitness = ImageFitness::new(&base);
+
+        let score = fitness.calculate(&target);
+
+        assert_eq!(score, (255 + 255) * 32 * 32);
+    }
+}
--- src/image2ga/mod.rs
+++ src/image2ga/mod.rs
@@ -1,2 +1,5 @@
 mod fitness;
-pub mod random;
+mod random;
+
+pub use fitness::ImageFitness;
+pub use random::{crossover, mutate, random_shape, random_u8};
