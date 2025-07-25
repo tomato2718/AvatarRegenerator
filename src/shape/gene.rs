4f73aaf45870f2b8024061010c3f37b495edcebb
tomato2718 <qweasdzxc030@gmail.com>
2025-07-25 18:01:36 +0800

    Fix: width, height MUST greater than 1

--- src/shape/gene.rs
+++ src/shape/gene.rs
@@ -15,8 +15,8 @@ impl Gene {
             !self.center.0 % self.boundary.0,
             !self.center.1 % self.boundary.1,
         );
-        self.width = !self.width % self.max_width;
-        self.height = !self.height & self.mex_height;
+        self.width = (!self.width % self.max_width) + 1;
+        self.height = (!self.height % self.mex_height) + 1;
         self.color = self.color.map(|c| !c);
     }
 }
--- src/shape/mod.rs
+++ src/shape/mod.rs
@@ -3,4 +3,7 @@ mod gene;
 mod rectangle;
 mod shape;
 
+pub use ellipse::Ellipse;
+pub use gene::Gene;
+pub use rectangle::Rectangle;
 pub use shape::GaShape;
