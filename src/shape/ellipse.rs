bff3c930ed999353f531b8e846db109aac6e7614
tomato2718 <qweasdzxc030@gmail.com>
2025-07-26 15:12:53 +0800

    Fix: Ellipse paint out of bound when pos=0, rad=1

--- src/shape/ellipse.rs
+++ src/shape/ellipse.rs
@@ -13,13 +13,13 @@ impl Ellipse {
     }
 
     pub fn place(&self, image: &mut Image<Rgba>) {
-        let boundary = (self.gene.width / 2, self.gene.height / 2);
+        let boundary = (max(self.gene.width / 2, 1), max(self.gene.height / 2, 1));
         let shape = ril::Ellipse::new()
             .with_position(
                 max(self.gene.center.0, boundary.0),
                 max(self.gene.center.1, boundary.1),
             )
-            .with_radii(max(self.gene.width / 2, 1), max(self.gene.height / 2, 1))
+            .with_radii(boundary.0, boundary.1)
             .with_fill(Rgba::new(
                 self.gene.color[0],
                 self.gene.color[1],
